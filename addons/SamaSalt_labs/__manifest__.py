47b4624be32349aadb77525b9150c73974e41a71
Muhammad Fahad <akontolive@gmail.com>
2022-05-25 19:08:23 +0400

    SamaSalt_lab added
    Salt Lab Reports

---
+++ addons/SamaSalt_labs/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Sama Salt Labs',
+    'category': 'Generic Modules/Human Resources',
+    'version': '15.0.1.0.0',
+    'author': 'Muhammad Fahad Munir',
+    'company': 'Akonto',
+    'maintainer': 'Akonto Solutions',
+    'website': 'https://www.akonto.ltd',
+    'summary': 'Manage your employee payroll records',
+    # 'images': ['static/description/banner.png'],
+    'description': "Odoo 15 Payroll, Payroll, Odoo 15,Odoo Payroll, Odoo Community Payroll",
+    'depends': [],
+    'data': [],
+    'demo': [],
+    # 'license': 'AGPL-3',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
